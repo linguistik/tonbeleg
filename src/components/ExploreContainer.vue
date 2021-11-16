aade237fc1c85feb2a66f14f73b80a624a09fad7
Nick <nicklemke@web.de>
2021-11-16 13:01:02 +0100

    add some functinality to record audio

--- src/components/ExploreContainer.vue
+++ src/components/ExploreContainer.vue
@@ -40,4 +40,8 @@ export default {
 #container a {
   text-decoration: none;
 }
+
+#ion-icon {
+  color: red;
+}
 </style>
--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -6,17 +6,17 @@
 
         <ion-tab-button tab="tab1" href="/tabs/tab1">
           <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-label>Gespeichert</ion-label>
         </ion-tab-button>
           
         <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-label>Aufnehmen</ion-label>
         </ion-tab-button>
         
         <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-label>Account</ion-label>
         </ion-tab-button>
 
       </ion-tab-bar>
