49448b4d79b0513f7533dab4b402ed219c6cdcdc
Jan Cramer <61551286+McBattery@users.noreply.github.com>
2021-11-19 17:21:03 +0100

    kleine änderungen

--- src/views/TabRecord.vue
+++ src/views/TabRecord.vue
@@ -3,46 +3,130 @@
     <PageHeader v-bind:title="t('general.appname')" />
 
     <ion-content :fullscreen="true">
-
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Aufnehmen</ion-title>
+          <ion-title size="large">Tab 3</ion-title>
         </ion-toolbar>
       </ion-header>
-      
-      <ExploreContainer name="Aufnehmen" />
-    </ion-content>
+      <!--  Hier ist der Button in der Mitte  -->
+      <ion-fab vertical="center" horizontal="center" slot="fixed">
+          Aufnahme starten
+        <ion-fab-button v-if="!recordingStatus" color="success" v-on:click="startRecordingTrigger()">
+          <ion-icon :name="caretForwardOutline"></ion-icon>
+        </ion-fab-button>
+        <ion-fab-button v-else color="danger" v-on:click="stopRecordingTrigger()">
+          <ion-icon :name="stopSharp"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
 
+
+    </ion-content>
   </ion-page>
 </template>
 
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import PageHeader from '@/components/layout/PageHeader.vue';
 
 import { 
-  IonPage, IonHeader, IonToolbar, IonTitle, IonContent
+  IonPage, IonHeader, IonToolbar, IonTitle, IonContent,
+  IonFab, IonFabButton, IonIcon,
 } from '@ionic/vue';
 
-import ExploreContainer from '@/components/ExploreContainer.vue';
+
+import { 
+  VoiceRecorder, 
+  GenericResponse, 
+  RecordingData, 
+  // VoiceRecorderPlugin, 
+  // CurrentRecordingStatus 
+} from 'capacitor-voice-recorder';
+
+import { 
+  caretForwardOutline,
+  stopSharp
+} from 'ionicons/icons';
 
 
 export default defineComponent({
-  name: 'Tab2',
+  name: 'Tab3',
 
   components: { 
     PageHeader, 
     IonHeader, IonToolbar, IonTitle, IonContent, IonPage,
-    ExploreContainer
+    IonFab, IonFabButton, IonIcon,
   },
 
   setup(){
     // multi-lingual support
     const { t } = useI18n();
 
-    return { t }
+
+    // Alles von hier: https://github.com/tchvu3/capacitor-voice-recorder#usage
+
+    // Überprüfe ob ein Mic existiert.
+    // z.B. Zeige eine Nachricht in der App an, dass ein Mic angeschlossen werden muss.
+    VoiceRecorder.canDeviceVoiceRecord()
+    .then((result: GenericResponse) => {
+      console.log(`Gibt es ein Mic? ${result.value}`)
+    });
+
+    // Der Browser fragt den Benutzer ob das Mikrofon aktiviert werden darf. 
+    // Das ist so ein kleines PopUp-Fenster im Browser.
+    VoiceRecorder.requestAudioRecordingPermission()
+    .then((result: GenericResponse) => {
+      console.log(`Ist das Mic eingeschaltet? ${result.value}`)
+    });
+
+    // 
+    VoiceRecorder.hasAudioRecordingPermission()
+    .then((result: GenericResponse) => {
+      console.log(`Darf die App aufzeichnen? ${result.value}`)
+    });
+
+    // Start & Stop Recoding
+    const recordingStatus = ref(false);  // Für Button
+
+    const startRecordingTrigger = async () => {
+      await VoiceRecorder.startRecording()
+        .then((result: GenericResponse) => {
+          recordingStatus.value = true
+          console.log(result.value)
+        })
+        .catch(error => {
+          recordingStatus.value = false
+          // "MISSING_PERMISSION", "ALREADY_RECORDING", "MICROPHONE_BEING_USED", "DEVICE_CANNOT_VOICE_RECORD", or "FAILED_TO_RECORD"
+          console.log(error)
+        });
+    }
+
+    const stopRecordingTrigger = async () => {
+      await VoiceRecorder.stopRecording()
+        .then((result: RecordingData) => {
+          recordingStatus.value = false
+          console.log(result.value)
+        })
+        .catch(error => {
+          recordingStatus.value = false
+          // "RECORDING_HAS_NOT_STARTED" or "FAILED_TO_FETCH_RECORDING"
+          console.log(error)
+        });
+    }
+
+    // VoiceRecorder.pauseRecording
+
+    // VoiceRecorder.resumeRecording
+
+    // Abspielen: https://github.com/tchvu3/capacitor-voice-recorder#playback
+
+
+    return { 
+      t, 
+      recordingStatus, startRecordingTrigger, stopRecordingTrigger,
+      caretForwardOutline, stopSharp
+    }
   }
 });
 </script>
