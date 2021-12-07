16fcbc20814ad880cbe52dc41c018abff9fad31f
matzienski <markusjulian99@gmail.com>
2021-12-07 17:11:46 +0100

    jedes Recording hat ein license attribut

--- src/scripts/RecordingData.ts
+++ src/scripts/RecordingData.ts
@@ -5,12 +5,14 @@ export default class RecordingData{
     parts: string[];
     length: number;
     upload: boolean;
+    license: string;
 
-    constructor(timestamp: number, name: string, parts: string[], length: number, upload: boolean){
+    constructor(timestamp: number, name: string, parts: string[], length: number, upload: boolean, license: string){
         this.timestamp = timestamp;
         this.name = name;
         this.parts = parts;
         this.length = length;
         this.upload = upload;
+        this.license = license;
     }
 }
