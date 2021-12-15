ca16b11c29991e1527e6b452963477073cfbecd0
Nick <46937156+nickl1234567@users.noreply.github.com>
2021-12-15 22:34:21 +0100

    Revert "Dev nick uploaded tab"

--- src/views/TabAccount.vue
+++ src/views/TabAccount.vue
@@ -5,133 +5,42 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Hochgeladenes</ion-title>
+          <ion-title size="large">Tab 3</ion-title>
         </ion-toolbar>
       </ion-header>
-      <ion-refresher slot="fixed" @ionRefresh="refresh">
-        <ion-refresher-content> </ion-refresher-content>
-      </ion-refresher>
-
-      <ion-list lines="full" v-if="uploadedRecordings.length != 0">
-        <Recording
-          v-for="item in uploadedRecordings"
-          v-bind:key="item"
-          v-bind:recording="item"
-          v-bind:provideFunctionality="false"
-        >
-        </Recording>
-      </ion-list>
-      <div id="container" v-else>
-        <p>{{ errorMessage }}</p>
-      </div>
+      
+      <ExploreContainer name="Tab 3 page" />
     </ion-content>
   </ion-page>
 </template>
 
 
 <script lang="ts">
-import { defineComponent, ref, Ref } from "vue";
-import { useI18n } from "vue-i18n";
-import PageHeader from "@/components/layout/PageHeader.vue";
+import { defineComponent } from 'vue';
+import { useI18n } from 'vue-i18n';
+import PageHeader from '@/components/layout/PageHeader.vue';
 
-import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonRefresher,
-  IonRefresherContent,
-  IonList,
-} from "@ionic/vue";
+import { 
+  IonPage, IonHeader, IonToolbar, IonTitle, IonContent
+} from '@ionic/vue';
 
-import RecordingData from "@/scripts/RecordingData";
-import Recording from "@/components/Recording.vue";
+import ExploreContainer from '@/components/ExploreContainer.vue';
 
-import firebase from "@/backend/firebase-config";
 
 export default defineComponent({
-  name: "TabAccount",
+  name: 'TabAccount',
 
-  components: {
-    PageHeader,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonPage,
-    IonRefresher,
-    IonRefresherContent,
-    IonList,
-    Recording,
+  components: { 
+    PageHeader, 
+    IonHeader, IonToolbar, IonTitle, IonContent, IonPage,
+    ExploreContainer
   },
 
-  setup() {
+  setup(){
     // multi-lingual support
     const { t } = useI18n();
 
-    const uploadedRecordings: Ref<RecordingData[]> = ref([]);
-    const errorMessage = ref(
-      "Du hast auf diesem Account keine gespeicherten Aufnahmen oder du bist nicht mit dem Internet verbunden"
-    );
-
-    const loadRecordingsFromDatabase = async () => {
-      const buffer: RecordingData[] = [];
-
-      const db = firebase.firestore();
-      const currentUser = firebase.auth().currentUser;
-      if (currentUser == null) return;
-      const userUID = currentUser.uid;
-
-      await db
-        .collection("users")
-        .doc(userUID)
-        .collection("recordings")
-        .get()
-        .then((collection) => {
-          collection.forEach((doc) => {
-            buffer.push(
-              new RecordingData(
-                doc.get("timestamp"),
-                doc.get("name"),
-                ["first"], //create array with length = doc.get("data").length();
-                doc.get("length"),
-                false,
-                false,
-                doc.get("license"),
-                doc.get("userID")
-              )
-            );
-          });
-        });
-
-      uploadedRecordings.value = buffer;
-    };
-    const refresh = async (event: any) => {
-      await loadRecordingsFromDatabase();
-      event.target.complete();
-    };
-
-    loadRecordingsFromDatabase();
-
-    return { t, refresh, uploadedRecordings, errorMessage };
-  },
+    return { t }
+  }
 });
 </script>
-<style scoped>
-#container {
-  text-align: center;
-  position: relative;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  color: #8c8c8c;
-  margin: 0;
-}
-</style>
