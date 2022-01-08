743f439778f8aeb3e3b27c420644e2469e3cbd50
Nick <nicklemke@web.de>
2022-01-08 01:04:32 +0100

    audio can now be cutted and the changes get stored locally. the actual data does not get cut already even thought the functionality is already implemented

--- src/views/editing/EditMainView.vue
+++ src/views/editing/EditMainView.vue
@@ -3,23 +3,68 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
+          <ion-title size="large">Schneiden</ion-title>
         </ion-toolbar>
       </ion-header>
-      {{ folderName }}
-      <ion-button @click="goBack()"> Go Back </ion-button>
-      <ion-button @click="load()"> Load </ion-button>
-      <ion-button @click="play()"> Play </ion-button>
-      <ion-button @click="finish()"> Finish </ion-button>
-      <ExploreContainer name="Editing" />
+      <ion-button expand="block" @click="finishWithoutSaving"> Beenden ohne Speichern</ion-button>
+      <ion-button expand="block" @click="finishAndSaveEventHandler()"> Beenden und Speichern</ion-button>
       <div id="waveform" class="waveform"></div>
+      <div id="timeline" class="timeline"></div>
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <ion-icon
+              :icon="playBackCircleOutline"
+              @click="skipEventHandler(-10)"
+            ></ion-icon>
+          </ion-col>
+          <ion-col>
+            <ion-icon
+              :icon="playSkipForwardCircleOutline"
+              @click="playPauseEventHandler"
+            ></ion-icon>
+          </ion-col>
+          <ion-col>
+            <ion-icon
+              :icon="playForwardCircleOutline"
+              @click="skipEventHandler(10)"
+            ></ion-icon>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+      <ion-grid>
+        <ion-row v-for="item in regionsRef" v-bind:key="item">
+          <ion-col size="1">
+            <ColoredIcon :icon="square" size="small" :color="usefulColorMapRef.get(item.color)"></ColoredIcon>
+          </ion-col>
+          <ion-col>
+            <ion-label class="elementName"> 
+              {{item.id }}
+            </ion-label>
+          </ion-col>
+          <ion-col size="2">
+            <ion-icon
+              :icon="playCircleOutline"
+              class="controlElements"
+              @click="item.play(0)"
+            ></ion-icon>
+          </ion-col>
+          <ion-col size="2">
+            <ion-icon
+              :icon="trashOutline"
+              class="controlElements"
+              @click="item.remove()"
+            ></ion-icon>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, Ref } from "vue";
 import { useI18n } from "vue-i18n";
 
 import {
@@ -29,29 +74,39 @@ import {
   IonTitle,
   IonContent,
   IonButton,
+  IonRow,
+  IonCol,
+  IonGrid,
+  IonIcon,
+  IonLabel,
+  alertController
 } from "@ionic/vue";
 
-import ExploreContainer from "@/components/ExploreContainer.vue";
+import {
+  playSkipForwardCircleOutline,
+  playBackCircleOutline,
+  playForwardCircleOutline,
+  square,
+  trashOutline,
+  playCircleOutline,
+} from "ionicons/icons";
+
+import ColoredIcon from "@/components/ColoredIcon.vue";
+
 import router from "@/router";
 import firebase from "@/backend/firebase-config";
 
-import WaveSurfer from "wavesurfer.js";
-import RegionPlugin from "wavesurfer.js/dist/plugin/wavesurfer.regions.js";
-import ExportAudioPlugin from "wavesurfer-export-audio-plugin";
+import WaveSurfer from "wavesurfer.js"; //BSD-3 ok
+import RegionPlugin from "wavesurfer.js/dist/plugin/wavesurfer.regions.js"; //ok
+import TimelinePlugin from "wavesurfer.js/dist/plugin/wavesurfer.timeline.js";
+import ExportAudioPlugin from "wavesurfer-export-audio-plugin"; //MIT ok
+
+import toWav from "audiobuffer-to-wav"; //MIT ok
+
 import { Filesystem, Directory, Encoding } from "@capacitor/filesystem";
+import {saveRegions, importRegions} from "@/scripts/editing/RegionSaver";
 
-//import encode from "opus-encode";
-//import EmsWorkerProxy from "opusenc.js/worker/EmsWorkerProxy";
-//import{ OpusEncoder }from "@discordjs/opus";
-//import  OpusScript from "opusscript";
-//import concat from "concat-stream";
-//import encode from "opus-encode/lib/encoder.js";
-//import * as encode from "opus-encode/lib/encoder.js";
-//import stream from "stream";
-//import audioEncoder from "audio-encoder";
-import toWav from "audiobuffer-to-wav";
-
-import { replayAudioData, getAudioString } from "@/scripts/ReplayData";
+import { replayAudioData, getAudioData } from "@/scripts/ReplayData";
 import { Region } from "wavesurfer.js/src/plugin/regions";
 
 export default defineComponent({
@@ -63,8 +118,13 @@ export default defineComponent({
     IonTitle,
     IonContent,
     IonPage,
-    ExploreContainer,
     IonButton,
+    IonRow,
+    IonCol,
+    IonGrid,
+    IonIcon,
+    ColoredIcon,
+    IonLabel,
   },
 
   props: {
@@ -75,14 +135,6 @@ export default defineComponent({
     // multi-lingual support
     const { t } = useI18n();
 
-    const str2ab = (str) => {
-      const buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-      const bufView = new Uint16Array(buf);
-      for (let i = 0, strLen = str.length; i < strLen; i++) {
-        bufView[i] = str.charCodeAt(i);
-      }
-      return buf;
-    };
     //https://github.com/mmig/opus-encdec
     //https://github.com/Rillke/opusenc.js
     //https://github.com/zhuker/lamejs
@@ -97,183 +149,231 @@ export default defineComponent({
     let wavesurfer: WaveSurfer;
     let regionID: string;
 
-    const regionIds: string[] = [];
+    let changesApplied = false;
+    const regionIdsRef: Ref<string[]> = ref([]);
+    const regionsRef: Ref<Region[]> = ref([]);
     let renderedBuffer;
-      const currentUser = firebase.auth().currentUser;
-      if (currentUser == null) return;
-      const currentUserUID = currentUser.uid;
+    const currentUser = firebase.auth().currentUser;
+    if (currentUser == null) return;
+    const currentUserUID = currentUser.uid;
 
+    let index = 0; //this is the index for determining the next color
+    const usefulColorMap = new Map<string, string>([
+      ["rgba(255,0,0,0.5)", "red"],
+      ["rgba(38,255,0,0.5)", "green"],
+      ["rgba(0,0,255,0.5)", "blue"],
+      ["rgba(255,0,230,0.5)", "pink"],
+      ["rgba(255,242,0,0.5)", "yellow"],
+      ["rgba(0,255,200,0.5)", "cyan"],
+      ["rgba(119,0,255,0.5)", "purple"],
+      ["rgba(0,0,0,0.5)", "black"],
+    ]);
+
+    const usefulColorMapRef = ref(usefulColorMap);
+
+    /**
+     * creates the wavesurfer
+     * loads the specific audio data
+     * initializes the renferedBuffer with an AudioBuffer of the audio data
+     *
+     * NOTE: this cannot be called directly on creation, since wavesurfer needs the specific waveform container in the html
+     */
     const load = async () => {
       wavesurfer = WaveSurfer.create({
         container: "#waveform",
         waveColor: "violet",
         progressColor: "purple",
-        plugins: [RegionPlugin.create(), ExportAudioPlugin.create()],
+        mediaControls: true,
+        plugins: [
+          RegionPlugin.create(),
+          ExportAudioPlugin.create(),
+          TimelinePlugin.create({
+            container: "#timeline",
+          }),
+        ],
       });
-
-      console.log(wavesurfer.RegionsPlugin);
-
-
       //TODO for all parts
 
       const audio = await replayAudioData(props.folderName, currentUserUID);
-      const audioString = await getAudioString(
-        props.folderName,
-        currentUserUID
-      );
+
       wavesurfer.load(audio);
 
       wavesurfer.on("ready", async () => {
         console.log("ready");
         wavesurfer.enableDragSelection({});
+        importRegions(wavesurfer, props.folderName);
         renderedBuffer = await wavesurfer.getRenderedAudioBuffer();
-        console.log("buffer:", renderedBuffer);
       });
 
       wavesurfer.on("region-created", (newRegion) => {
-        console.log("new Region");
-        regionIds.push(newRegion.id);
+        newRegion.color = Array.from(usefulColorMap.keys())[index % usefulColorMap.size];
+        
+        index = index + 1;
+        regionIdsRef.value.push(newRegion.id);
+        regionsRef.value.push(newRegion);
       });
 
-      wavesurfer.on("region-update-end", (newRegion) => {
-        console.log(newRegion.start, ", ", newRegion.end);
-        regionID = newRegion.id;
+      wavesurfer.on("region-update-end", (region) => {
+        changesApplied = true;
       });
-    };
 
-    const play = () => {
-      //wavesurfer.regions.list[regionID].play();
-      wavesurfer.play();
-    };
+      wavesurfer.on("region-removed", (region) => {
+        const index = regionsRef.value.indexOf(region);
+        if (index > -1) {
+          changesApplied = true;
+          regionsRef.value.splice(index, 1);
+        }
+      });
+    }; //load
 
-    //return a new ogg encoded file containing the data of that region
+    const loadAfterTimeout = async () => {
+      window.setTimeout(load,100);
+    }
+    loadAfterTimeout();
+    /**
+     * copys the specific region of the renderedBuffer and encodes it to a wave file format
+     * @param {Region} region the region, we want the data from
+     * @returns {ArrayBuffer} the encoded data in wave format
+     */
     const trimAudio = (region: Region) => {
       console.log("trim", region);
       //https://github.com/Vinit-Dantkale/AudioFy/blob/master/audio.js
-      const startPoint = Math.floor((region.start*renderedBuffer.length)/wavesurfer.getDuration());
-      const endPoint = Math.ceil((region.end*renderedBuffer.length)/wavesurfer.getDuration());
+      const startPoint = Math.floor(
+        (region.start * renderedBuffer.length) / wavesurfer.getDuration()
+      );
+      const endPoint = Math.ceil(
+        (region.end * renderedBuffer.length) / wavesurfer.getDuration()
+      );
       const audioBufferLength = endPoint - startPoint;
       const trimmedAudio = new AudioContext().createBuffer(
-        1,//Channels
+        1, //Channels
         audioBufferLength,
         renderedBuffer.sampleRate
       );
-      //copy channel data 
-      trimmedAudio.copyToChannel(renderedBuffer.getChannelData(0).slice(startPoint, endPoint), 0);
-      
-      //trimmedAudio is the buffer thats needs to be encoded to ogg for returning
-     /* const blob = new Blob([trimmedAudio.getChannelData(0)],{type: "audio/wav" });
-      const audio = new Audio();
-      const url = window.URL.createObjectURL(blob);
-      audio.src = url;
-      audio.load();
-      audio.oncanplay = ()=>{console.log("play");audio.play();}*/
+      //copy channel data
+      trimmedAudio.copyToChannel(
+        renderedBuffer.getChannelData(0).slice(startPoint, endPoint),
+        0
+      );
 
-/*      const context = new AudioContext();
-      const source = context.createBufferSource();
-      source.buffer = trimmedAudio;
-      source.connect(context.destination);
-      source.start();
-*/
-      //const worker = new Worker("opus-encdec/dist/encoderWorker.js");
-//console.log(worker);
-      //console.log(new OpusEncoder());
-     /* const args = ["input", "output.opus"];
-      const outData = {"encoded.opus":{"MIME":"audio/ogg"}};
-
-      const worker = new Worker("opusenc.js/worker/EmsWorkerProxy.js");
-      worker.postMessage({command: "encode",
-      args:args,
-      outData: outData,
-      fileData: trimmedAudio.getChannelData(0)})*/
-
-      //const encoder = new OpusEncoder(48000,1);
-
-      //const encoded = encoder.encode(trimmedAudio);
-      //console.log(encoded);
-/*
-      console.assert(trimmedAudio.sampleRate == 48000);
-      console.assert(trimmedAudio.numberOfChannels == 1);
-      const encoder = new OpusScript(48000, 1, OpusScript.Application.AUDIO ,{wasm: false});
-      const encodedAudio = encoder.encode(trimmedAudio.getChannelData(0), 48000 * 20 /1000);
-      //console.log(encodedAudio);
-*/
-
-/*      const audioStream = new stream.Readable({objectMode: true}); 
-
-      console.log(audioStream);
-      audioStream._read = function(){
-        this.push(trimmedAudio);
-        this.push(null);
-      }
-      const doSomething = concat(function(buf){
-        const blob = new Blob([buf.toArrayBuffer()]);
-        console.log(blob);
-      });
-      console.log(encode);
-      //audioStream.pipe(encode());//.pipe(doSomething);
-*/
-/*
-      console.log("start");//only works with 44.1kHz, 32kHz, 24kHz. we have 44kHz
-      audioEncoder(trimmedAudio, 128, function onProgress(){console.log("progress");}, function onComplete(blob){
-        console.log(blob);
-      });
-*/
       const wav = toWav(trimmedAudio);
       return wav;
+    }; //trimAudio
 
-      
-    };
-
-    const playAudioBuffer = (buf) =>{
-      new AudioContext().decodeAudioData(buf, function(decodedBuffer){
-          const context = new AudioContext();
-      const source = context.createBufferSource();
-      source.buffer = decodedBuffer;
-      source.connect(context.destination);
-      source.start();
+    /**
+     * plays an audio buffer
+     * @param {AudioBuffer} buf the buffer to be played
+     */
+    const playAudioBuffer = (buf) => {
+      new AudioContext().decodeAudioData(buf, function (decodedBuffer) {
+        const context = new AudioContext();
+        const source = context.createBufferSource();
+        source.buffer = decodedBuffer;
+        source.connect(context.destination);
+        source.start();
       });
-    }
+    }; //play audio buffer
 
-    const finish = async () => {
-      for (const regionId of regionIds) {
-        console.log(regionId);
-        const wavFileData = trimAudio(wavesurfer.regions.list[regionId]);
+    const finishAndSaveEventHandler = async () => {
+      saveRegions(regionsRef.value, props.folderName);
+      
+      //this is still useful code!!!
+      /*for (const region of regionsRef.value) {
+        const wavFileData = trimAudio(region);
         await Filesystem.writeFile({
-          path: "/" + currentUserUID + "/" + props.folderName + "/" + "export.wav",
+          path:
+            "/" + currentUserUID + "/" + props.folderName + "/" + region.id +".wav",
           data: wavFileData,
           directory: Directory.Data,
-          encoding: Encoding.UTF8 
+          encoding: Encoding.UTF8,
         });
-      }
-      const readRes = await Filesystem.readFile({
-        path: "/" + currentUserUID + "/" + props.folderName + "/" + "export.wav",
-        directory: Directory.Data,
-        encoding: Encoding.UTF8,
-      });
-      console.log("read", readRes.data);
-      console.log(typeof(readRes.data));
-
-      playAudioBuffer(readRes.data);
+      }*/
+      //playAudioBuffer(await getAudioData(props.folderName, "export.wav"));
 
 
-      /*new AudioContext().decodeAudioData(readRes.data, function(buffer){
-        console.log(buffer);
-      })*/
-/*
-      const context = new AudioContext();
-      const source = context.createBufferSource();
-      source.buffer = readRes.data;
-      source.connect(context.destination);
-      source.start();
-*/
-     // wavesurfer.load(readRes.data);
-    };
 
-    const goBack = () => {
       router.back();
+    }; //finish
+
+
+    const playPauseEventHandler = () => {
+      wavesurfer.playPause();
     };
-    return { t, goBack, load, finish, play };
+
+    const skipEventHandler = (amt: number) => {
+      wavesurfer.skip(amt);
+    };
+
+    const finishWithoutSaving = async () => {
+      if(changesApplied){
+        //ask user if he wants to leave without saving
+        const alert = await alertController.create({
+        message: "Willst du wirklich abbrechen ohne zu speichern?.",
+        buttons: [
+          {
+            text: "Ja",
+            handler: () => {
+              router.back();
+              wavesurfer.destroy();
+            },
+          },
+          {
+            text: "Nein",
+            handler: () => {
+              //do nothing, stay in this window
+            },
+          },
+        ],
+      });
+      await alert.present();
+      }else{
+        router.back();
+        wavesurfer.destroy();
+      }
+    };
+    return {
+      t,
+      finishWithoutSaving,
+      finishAndSaveEventHandler,
+      playPauseEventHandler,
+      skipEventHandler,
+      regionsRef,
+      usefulColorMapRef,
+      playSkipForwardCircleOutline,
+      playBackCircleOutline,
+      playForwardCircleOutline,
+      square,
+      trashOutline,
+      playCircleOutline,
+    };
   },
 });
 </script>
+<style scoped>
+.waveform {
+  margin: 25px;
+  border-style: solid;
+  border-width: thin;
+  border-radius: 5px;
+}
+.timeline {
+  margin: 25px;
+}
+
+.controlElements{
+  font-size: 32px;
+}
+
+
+ion-icon {
+  font-size: 64px;
+}
+
+ion-col {
+  text-align: center;
+}
+
+ion-grid {
+  margin-bottom: 30px;
+}
+</style>
