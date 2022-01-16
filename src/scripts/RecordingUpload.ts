4133c12293285b175f7819006695327349caa38c
Nick <nicklemke@web.de>
2022-01-17 00:44:58 +0100

    fix #137

--- src/scripts/RecordingUpload.ts
+++ src/scripts/RecordingUpload.ts
@@ -6,11 +6,16 @@ import {
     loadRecordings,
     removeRecordingEntry
 } from "@/scripts/RecordingStorage";
-import {getWifi} from "@/scripts/UserSettingsStorage";
-import {Filesystem, Directory, Encoding} from "@capacitor/filesystem";
-import {loadUserSettings} from "@/scripts/UserSettingsStorage";
+import { getWifi } from "@/scripts/UserSettingsStorage";
+import { Filesystem, Directory, Encoding } from "@capacitor/filesystem";
+import { loadUserSettings } from "@/scripts/UserSettingsStorage";
 import RecordingData from "@/scripts/RecordingData";
-import {ConnectionStatus, Network} from "@capacitor/network";
+import { ConnectionStatus, Network } from "@capacitor/network";
+
+import { trimAudio, playArrayBuffer, playAudioBuffer } from "./editing/AudioUtils";
+import { getAudioData } from "@/scripts/ReplayData";
+
+import { arrayBufferToBase64String } from "./Base64Utils";
 
 export const RecordingUploadArray: RecordingData[] = [];
 //export let inRecordingArrayIdent: string[][] = [];
@@ -43,15 +48,15 @@ export function deleteFromUploadArray(ident: string){
 }
 
 
-*/export async function deleteAllFromUploadArray(){
+*/export async function deleteAllFromUploadArray() {
     RecordingUploadArray.splice(0,)
 }
 
-export async function deleteUploadedRecordsFromDevice(){
-        for (i = 0; i < RecordingUploadArray.length; i = i + 1) {
-            await removeRecordingEntry(RecordingUploadArray[i]);
-        }
-        RecordingUploadArray.splice(0, )
+export async function deleteUploadedRecordsFromDevice() {
+    for (i = 0; i < RecordingUploadArray.length; i = i + 1) {
+        await removeRecordingEntry(RecordingUploadArray[i]);
+    }
+    RecordingUploadArray.splice(0,)
 }
 
 
@@ -60,9 +65,9 @@ export async function UploadToFirebase() {
     const currentUser = firebase.auth().currentUser;
     if (currentUser == null) return;
     const networkStatus = await Network.getStatus().then(result => result);
-    console.log("network Status:" ,networkStatus);
+    console.log("network Status:", networkStatus);
 
-    if(getWifi()==true && networkStatus.connectionType != 'wifi'){
+    if (getWifi() == true && networkStatus.connectionType != 'wifi') {
         console.log("Upload not possible due to wrong Connection Type")
         return;
     }
@@ -70,36 +75,49 @@ export async function UploadToFirebase() {
     const RecordingUserID = [];
     const RecordingID = [];
     for (i = 0; i < RecordingDataArr.length; i = i + 1) {
-        if (RecordingDataArr[i].upload == false && RecordingDataArr[i].selectedForUpload == true) {
+        if (RecordingDataArr[i].upload == false && RecordingDataArr[i].selectedForUpload == true) {//not already uploaded, but selected for upload
             await db.collection("users").doc(currentUser.uid).collection("recordings").doc(RecordingDataArr[i].timestamp.toString()).set({
-                data: (await Filesystem.readFile({
-                    path: "/" + currentUser.uid + "/" + RecordingDataArr[i].timestamp + "/" + "0.raw",
-                    directory: Directory.Data,
-                    encoding: Encoding.UTF8,
-                })).data,
                 userID: RecordingDataArr[i].userID,
                 timestamp: RecordingDataArr[i].timestamp,
                 name: RecordingDataArr[i].name,
                 length: RecordingDataArr[i].length,
                 license: RecordingDataArr[i].license,
-            }, {merge: true});
-            RecordingUploadArray.push(RecordingDataArr[i]);
+            }, { merge: true });
+
+            const recordingDataI = RecordingDataArr[i];
+            getAudioData(RecordingDataArr[i].timestamp, "0.raw", async (completeAudioBuffer) => {
+
+                //playAudioBuffer(completeAudioBuffer);
+                for (let j = 0; j < recordingDataI.parts.length; j++) {
+                    const part = recordingDataI.parts[j];
+                    const cuttedArrayBuffer: ArrayBuffer = trimAudio(part,completeAudioBuffer);
+                    //https://stackoverflow.com/questions/9267899/arraybuffer-to-base64-encoded-string
+                    const base64String = arrayBufferToBase64String(cuttedArrayBuffer);
+
+                    await db.collection("users").doc(currentUser.uid).collection("recordings")
+                        .doc(recordingDataI.timestamp.toString()).collection("parts").doc(part.id).set({
+                            data: base64String,
+                            name: part.name,
+                        })
+                }
+                RecordingUploadArray.push(recordingDataI);
+            });
         }
     }
 
     console.log("wrote to database");
     //deleteAllFromUploadArray();
-
-    for(i=0; i<RecordingDataArr.length; i=i+1){
-        if(RecordingDataArr[i].upload == false && RecordingDataArr[i].selectedForUpload == true)
-        await setRecordingEntryUploadBoolean(RecordingDataArr[i].timestamp, true);
+    
+    for (i = 0; i < RecordingDataArr.length; i = i + 1) {
+        if (RecordingDataArr[i].upload == false && RecordingDataArr[i].selectedForUpload == true)
+            await setRecordingEntryUploadBoolean(RecordingDataArr[i].timestamp, true);
     }
     //deleteAllFromUploadArray();
 }
 
-export async function isConnected(){
+export async function isConnected() {
     const Status = await Network.getStatus().then(result => result);
-    console.log("is Connected?:" ,Status.connected);
+    console.log("is Connected?:", Status.connected);
     return Status.connected;
 }
 /*export function setUploadArrays(uploadArray: string[], alreadyUp: string[][], uploadArrayIdent: string[][]){
